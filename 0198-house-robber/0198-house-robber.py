f40663b1ebcaacc6fc6a2d3bcf9d9469ef7f72e5
Sreeja Oruganti <orugantisreeja06@gmail.com>
2026-07-21 16:33:53 +0530

    Time: 0 ms (100%), Space: 19.3 MB (23.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,23 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n=len(nums)
-        dp=[-1] * (n+1)
         if n==1:
             return nums[0]
-        if n==2:
-            return max(nums[0],nums[1])
-        return self.ways(n,nums,dp)
-    def ways(self,n,nums,dp):
-        if n==1:
-            return nums[0]
-        if n==2:
-            return max(nums[0],nums[1])
-        if dp[n]!=-1:
-            return dp[n]
-        dp[n]=max(nums[n-1]+self.ways(n-2,nums,dp),self.ways(n-1,nums,dp))
-        return dp[n]
-
+        dp=[-1] * (n)
+        dp[0]=nums[0]
+        dp[1]=max(nums[0],nums[1])
+        for i in range(2,n):
+            dp[i]=max(nums[i]+dp[i-2],dp[i-1])
+        return dp[n-1]
+       
         
-    
-
         
