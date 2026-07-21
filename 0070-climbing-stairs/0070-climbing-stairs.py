a095f82f279168b2f1693c322bf8a4214fc594c3
Sreeja Oruganti <orugantisreeja06@gmail.com>
2026-07-21 12:33:43 +0530

    Time: 0 ms (100%), Space: 19.3 MB (16.76%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -5,18 +5,7 @@ class Solution:
         dp=[-1] * (n+1)
         dp[0]=1
         dp[1]=1
-        self.ways(n,dp)
+        for i in range(2,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
         return dp[n]
-
-
-
-
-    def ways(self,n,dp):
-        if n<=1:
-            return dp[n]
-        if dp[n]!=-1:
-            return dp[n]
-        dp[n]=self.ways(n-1,dp)+self.ways(n-2,dp)
-        return dp[n]
- 
         
