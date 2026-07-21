99abe02d7297cab97db9dd609462f23188e25b2e
Sreeja Oruganti <orugantisreeja06@gmail.com>
2026-07-21 16:36:10 +0530

    Time: 0 ms (100%), Space: 19.2 MB (61.15%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,7 +3,7 @@ class Solution:
         n=len(nums)
         if n==1:
             return nums[0]
-        dp=[-1] * (n)
+        
         prev2=nums[0]
         prev1=max(nums[0],nums[1])
         for i in range(2,n):
