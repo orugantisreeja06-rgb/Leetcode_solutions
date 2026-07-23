d3ab1d907f3b1c6a5995ab127eb7a8c4d326f1fe
Sreeja Oruganti <orugantisreeja06@gmail.com>
2026-07-23 06:36:44 +0530

    Time: 15 ms (44.44%), Space: 19.4 MB (75.18%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        dict1={}
+        for i in range(len(s)):
+            if s[i] not in dict1:
+                dict1[s[i]]=1
+            else:
+                dict1[s[i]]+=1
+        for i in range(len(t)):
+            if t[i] in dict1:
+                dict1[t[i]]-=1
+                if dict1[t[i]]==0:
+                    del dict1[t[i]]
+                
+        return len(dict1)==0
+
+        
